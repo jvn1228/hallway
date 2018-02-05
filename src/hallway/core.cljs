c79ba58ab49109ff9f62152f631dd233f1f867d2
jvn1228 <jvn1228@gmail.com>
2018-02-04 20:43:11 -0600

    Animate part 1

--- src/hallway/core.cljs
+++ src/hallway/core.cljs
@@ -15,3 +15,20 @@
     (set! (.-fillStyle ctx) color)
     (.fillRect ctx box-x box-y 100 100)))
 
+(defn erase-ctx
+	[]
+	(let [canvas (.getElementById js/document "canvas")
+        ctx (.getContext canvas "2d")]
+    (.clearRect ctx 0 0 (.-innerWidth js/window) (.-innerHeight js/window))))
+
+(defn animate-box
+	[init-x init-y timeout]
+	(loop [x init-x]
+		(when (< x (- (.-innerWidth js/window) 100))
+			(erase-ctx)
+			(draw-box "blue" x init-y)
+			(recur (inc x)))))
+			#_(js/setTimeout #(do 
+												(erase-ctx)
+												(recur (inc x)))
+										  timeout)
